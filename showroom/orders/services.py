c237dfc64f13a97dd943fc2df4f61da7a0f6c890
Alexander <amarjin6@gmail.com>
2022-08-09 20:48:30 +0300

    Remove bugs in processing orders

--- showroom/orders/services.py
+++ showroom/orders/services.py
@@ -3,7 +3,6 @@ from typing import Dict
 from dealer.models import Dealer, DealerCar
 from users.models import UserProfile, UserProfileCar
 from orders.models import CustomerOrder, DealerOrder
-from cars.models import Car
 from core.enums import Profile
 
 
@@ -28,66 +27,77 @@ def check_order(client_name: str, attrs: Dict) -> bool:
 def process_customer_order() -> bool:
     orders = CustomerOrder.objects.all()
     for order in orders:
-        customer_profile = UserProfile.objects.get(id=order.customer.id)
-        car_profile = Car.objects.get(id=order.car)
-        desired_amount = order['amount']
-        desired_price = order['price'] / desired_amount
-        dealer = DealerCar.objects.select_related('dealer').filter(price__lte=desired_price,
-                                                                   amount__gte=desired_amount,
-                                                                   car__id=car_profile.id).order_by('price').first()
+        if order.is_active:
+            customer_profile = order.customer
+            car_profile = order.car
+            desired_amount = order.amount
+            desired_price = order.price / desired_amount
+            dealer = DealerCar.objects.select_related('dealer').filter(price__lte=desired_price,
+                                                                       amount__gte=desired_amount,
+                                                                       car__id=car_profile.id).order_by('price').first()
 
-        if dealer:
-            final_price = dealer.price * desired_amount
+            if dealer:
+                final_price = dealer.price * desired_amount
 
-            dealer_balance = dealer.dealer.profile.profile_balance.only('amount').first()
-            if dealer_balance:
-                dealer_balance.amount += final_price
-                dealer.amount -= desired_amount
-                dealer_balance.save()
-                dealer.save()
+                dealer_balance = dealer.dealer.profile.profile_balance.only('amount').first()
+                if dealer_balance:
+                    dealer_balance.amount += final_price
+                    dealer.amount -= desired_amount
+                    dealer.save()
+                    dealer_balance.save()
 
-                customer_balance = customer_profile.profile_balance.only('amount').first()
-                customer_balance.amount -= final_price
-                customer_balance.save()
+                    customer_balance = customer_profile.profile_balance.only('amount').first()
+                    customer_balance.amount -= final_price
+                    customer_balance.save()
 
-            order.is_active = False
-            order.save()
+                order.is_active = False
+                order.save()
 
-            return True
+                return True
 
-        return False
+            return False
 
     return True
 
 
-def process_dealer_order(order: Dict) -> bool:
-    dealer_profile = Dealer.objects.get(id=order.get('dealer'))
-    car_profile = Car.objects.get(id=order.get('car'))
-    desired_amount = order['amount']
-    desired_price = order['price'] / desired_amount
-    vendor = UserProfileCar.objects.select_related('profile').filter(price__lte=desired_price,
-                                                                     amount__gte=desired_amount,
-                                                                     car__id=car_profile.id).order_by('price').first()
+def process_dealer_order() -> bool:
+    orders = DealerOrder.objects.all()
+    for order in orders:
+        if order.is_active:
+            dealer_profile = order.dealer
+            car_profile = order.car
+            desired_amount = order.amount
+            desired_price = order.price / desired_amount
+            vendor = UserProfileCar.objects.select_related('profile').filter(price__lte=desired_price,
+                                                                             amount__gte=desired_amount,
+                                                                             car__id=car_profile.id).order_by(
+                'price').first()
 
-    if vendor:
-        final_price = vendor.price * desired_amount
+            if vendor:
+                final_price = vendor.price * desired_amount
 
-        dealer_balance = dealer_profile.profile.profile_balance.only('amount').first()
-        dealer_balance.amount -= final_price
-        dealer_balance.save()
+                dealer_balance = dealer_profile.profile.profile_balance.only('amount').first()
+                dealer_balance.amount -= final_price
+                dealer_balance.save()
 
-        dealer_car = dealer_profile.dealer_dealer_car.only('amount').filter(car_id=car_profile.id).first()
-        if dealer_car:
-            dealer_car.amount += desired_amount
-            dealer_car.save()
+                dealer_car = dealer_profile.dealer_dealer_car.only('amount').filter(car_id=car_profile.id).first()
+                if dealer_car:
+                    dealer_car.amount += desired_amount
+                    dealer_car.save()
 
-        else:
-            new_car = DealerCar(dealer=dealer_profile, car=car_profile, amount=desired_amount, price=vendor.price * 1.1)
-            new_car.save()
+                else:
+                    new_car = DealerCar(dealer=dealer_profile, car=car_profile, amount=desired_amount,
+                                        price=vendor.price * 1.1)
+                    new_car.save()
 
-        vendor.amount -= desired_amount
-        vendor.save()
+                vendor.amount -= desired_amount
+                vendor.save()
 
-        return True
+                order.is_active = False
+                order.save()
 
-    return False
+                return True
+
+            return False
+
+    return True
--- showroom/orders/tasks.py
+++ showroom/orders/tasks.py
@@ -3,5 +3,10 @@ from orders.services import process_dealer_order, process_customer_order
 
 
 @app.task
-def buy_car():
+def buy_car_from_dealer():
     return True if process_customer_order() else False
+
+
+@app.task
+def buy_car_from_vendor():
+    return True if process_dealer_order() else False
