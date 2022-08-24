e8a42550f9cae82424825f34b6153cc331716358
Alexander <amarjin6@gmail.com>
2022-08-24 16:13:01 +0300

    Create fixtures

--- showroom/tests/test_order.py
+++ showroom/tests/test_order.py
@@ -21,6 +21,36 @@ def user_a(db) -> User:
 
 
 @pytest.fixture
+def user_b(db) -> User:
+    user = User.objects.create_user(
+        username="B",
+        password="secret",
+        first_name="Baloo",
+        last_name="Doe",
+        email="baloo@gmail.com",
+        is_staff=False,
+        is_superuser=False,
+        is_active=True,
+    )
+    return user
+
+
+@pytest.fixture
+def user_c(db) -> User:
+    user = User.objects.create_user(
+        username="C",
+        password="secret",
+        first_name="Lucas",
+        last_name="Doe",
+        email="lucas@gmail.com",
+        is_staff=False,
+        is_superuser=False,
+        is_active=True,
+    )
+    return user
+
+
+@pytest.fixture
 def customer_alex(db) -> UserProfile:
     customer = UserProfile(
         user=user_a,
@@ -29,5 +59,19 @@ def customer_alex(db) -> UserProfile:
     return customer
 
 
-def test_should_create_user_with_username(db) -> None:
-    assert user_a.username == 'A'
+@pytest.fixture
+def vendor_john(db) -> UserProfile:
+    vendor = UserProfile(
+        user=user_b,
+        profile=Profile.VENDOR.value,
+    )
+    return vendor
+
+
+@pytest.fixture
+def dealer_maria(db) -> UserProfile:
+    dealer = UserProfile(
+        user=user_c,
+        profile=Profile.DEALER.value,
+    )
+    return dealer
